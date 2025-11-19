bf7c3ccfa2c2e0b0cc93c9bf7f0e8bdb4c0e0cea
admin <admin@admins-MacBook-Pro.local>
2025-11-19 02:01:23 -0800

    add test case to check whether item is displayed in the list or not

--- src/components/ItemCard.test.tsx
+++ src/components/ItemCard.test.tsx
@@ -1,3 +1,5 @@
+import { render,screen } from "@testing-library/react-native";
+import ItemCard from "./ItemCard";
 
 const mockItem = {
   id: "1",
@@ -5,5 +7,13 @@ const mockItem = {
   price: 50,
   image: require("/Users/admin/Desktop/react-native/canteen-management-system/assets/images/snacks/burger.jpeg"), 
 };
+
+describe("ItemCard component",()=>{
+    test("renders given item ",()=>{
+        const {getByText} = render(<ItemCard item={mockItem}/>);
+        expect(getByText("Burger")).toBeOnTheScreen();
+    })
+})
+
 
 
