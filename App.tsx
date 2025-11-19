33c93fde9216e71df0c0628f52d706b4fdaf2656
admin <admin@MacBookPro.lan>
2025-11-18 22:19:27 -0800

    display all the items

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SectionLists from './src/components/SectionLists';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SectionLists/>
     </View>
   );
 }
