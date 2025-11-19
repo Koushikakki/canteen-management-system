5f6edc12bea5f7a7f028bedfb30bc3d06dcabd00
admin <admin@admins-MacBook-Pro.local>
2025-11-19 02:03:18 -0800

    add test case to check a section is displayed or not

---
+++ src/components/SectionLists.test.tsx
@@ -0,0 +1,13 @@
+
+
+const mockSections = [
+  {
+    id : 's1',
+    title: "Main Course",
+    data: [
+      { id: "1", title: "Pizza", price: 10, image: require("/Users/admin/Desktop/react-native/canteen-management-system/assets/images/snacks/pizza.jpeg") }
+    ],
+  },
+];
+
+
