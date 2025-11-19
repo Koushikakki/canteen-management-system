9cdec92c0514b0e81fa696f1220acd3165fbfd12
admin <admin@MacBookPro.lan>
2025-11-18 20:10:31 -0800

    add a sample data

---
+++ src/data/data.ts
@@ -0,0 +1,13 @@
+import { Section } from "../types/types";
+
+export const menuData : Section[] = [
+    {
+        id : 's1',
+        title : 'beverages',
+        data : [
+            {
+                id : 'b1', title : 'coffee', price : 50, image : require('/Users/admin/Desktop/react-native/canteen-management-system/assets/coffee.jpeg')
+            }
+        ]
+    }
+]
