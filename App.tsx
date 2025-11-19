6633e37e0b28924e28a2cb4fab96c8a0e091ecf2
admin <admin@admins-MacBook-Pro.local>
2025-11-19 00:40:42 -0800

    add styles for the app

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { menuData } from './src/data/data';
 export default function App() {
   return (
     <View style={styles.container}>
+      <View><Text style={styles.heading}>Everest Canteen</Text></View>
       <SectionLists sections={menuData}/>
     </View>
   );
@@ -14,7 +15,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    paddingTop: 50,
+    paddingBottom :70
+    },
+    heading : {
+      textAlign:'center',
+      fontSize : 28,
+      fontWeight : '700',
+      color: "#000001ff",
+      marginHorizontal : 16,
+      marginBottom : 20
+    }
 });
