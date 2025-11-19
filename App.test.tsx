c0856849de6d42ebde856baae860860117b3791e
admin <admin@admins-MacBook-Pro.local>
2025-11-19 02:16:35 -0800

    task mock data to test the app

---
+++ App.test.tsx
@@ -0,0 +1,14 @@
+const menuData = [
+  {
+    id: "s1",
+    title: "Snacks",
+    data: [
+      {
+        id: "",
+        title: "samosa",
+        price: 30,
+        image: require("/Users/admin/Desktop/react-native/canteen-management-system/assets/images/snacks/pizza.jpeg")
+      }
+    ]
+  }
+]
