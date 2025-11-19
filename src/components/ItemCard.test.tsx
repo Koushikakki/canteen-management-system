52bd2d9a2fbbf8a8607d72c28efe7abb32de436d
admin <admin@admins-MacBook-Pro.local>
2025-11-19 01:50:08 -0800

    take a mock item to test whether it is displaying or not

---
+++ src/components/ItemCard.test.tsx
@@ -0,0 +1,9 @@
+
+const mockItem = {
+  id: "1",
+  title: "Burger",
+  price: 50,
+  image: require("/Users/admin/Desktop/react-native/canteen-management-system/assets/images/snacks/burger.jpeg"), 
+};
+
+
